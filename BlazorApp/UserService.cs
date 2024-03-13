0c8bb454fd72eee39fcc3b9921bdfd3527cf680a
MrKazio <115210775+Dzban0@users.noreply.github.com>
2024-03-13 12:01:08 +0100

    Dodaj .gitattributes i .gitignore.

---
+++ BlazorApp/UserService.cs
@@ -0,0 +1,27 @@
+﻿namespace BlazorApp
+{
+    public class UserService
+    {
+        private Dictionary<string, string> _users = new Dictionary<string, string>();
+
+        public void Add(string connectionId, string username)
+        {
+            _users[username] = connectionId;
+        }
+
+        public void RemoveByName(string username)
+        {
+            _users.Remove(username);
+        }
+
+        public string GetConnectioIdByName(string username)
+        {
+            return _users.ContainsKey(username) ? _users[username] : null;
+        }
+
+        public IEnumerable<(string ConnectionId, string Username)> GetAll()
+        {
+            return _users.Select(u => (u.Value, u.Key));
+        }
+    }
+}
